f22d91f4fc21af2e7b03be67d69d3a7fd0f9be29
Justin Vrana <justin.vrana@gmail.com>
2019-06-20 10:51:15 -0700

    remove all

--- autoplanner/__version__.py
+++
@@ -1,9 +0,0 @@
-# Autoplanner
-
-__title__ = "Autoplanner"
-__description__ = "Automatic experiment planning"
-__url__ = ""
-__version__ = "0.0.4a"
-__author__ = "Justin Vrana"
-__author_email__ = "jvrana@uw.edu"
-__pypi__ = ""
