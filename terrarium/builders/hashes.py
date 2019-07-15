7f63f553dac05ceccfcb291ed12d23f30131f6be
Justin Vrana <justin.vrana@gmail.com>
2019-07-15 10:52:20 -0700

    typing for hashes.py

--- terrarium/builders/hashes.py
+++ terrarium/builders/hashes.py
@@ -1,13 +1,14 @@
 from terrarium import constants as C
 from uuid import uuid4
+from typing import Tuple
 
 
-def make_hash(*args, sep="-"):
+def make_hash(*args, sep="-") -> str:
     """Returns a hash string composed of several tokens"""
     return sep.join([str(_) for _ in args])
 
 
-def external_aft_hash(aft):
+def external_aft_hash(aft: dict) -> str:
     if not aft["field_type_id"]:
         return str(uuid4())
     return make_hash(
@@ -18,11 +19,11 @@ def external_aft_hash(aft):
     )
 
 
-def internal_aft_hash(aft):
+def internal_aft_hash(aft: dict) -> str:
     return make_hash(aft[C.MODEL_CLASS], aft["field_type"]["parent_id"])
 
 
-def edge_hash(pair):
+def edge_hash(pair: Tuple[dict, dict]) -> str:
     h = "{}->{}".format(external_aft_hash(pair[0]), external_aft_hash(pair[1]))
     return make_hash(
         pair[0]["field_type"]["parent_id"], h, pair[1]["field_type"]["parent_id"]
