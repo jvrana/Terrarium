8e7adf16e073bc7dd703eda8b1ebea37b144a95c
jvrana <justin.vrana@gmail.com>
2019-07-27 09:48:39 -0700

    update input file validation

---
+++ terrarium/utils/validator.py
@@ -0,0 +1,56 @@
+from validator import *
+
+
+class Any(Validator):
+    def __init__(self, *values):
+        self.values = values
+
+        err_msg = " OR ".join(
+            "({}) {}".format(i, v.err_message) for i, v in enumerate(self.values)
+        )
+        not_msg = " AND ".join(
+            "({}) {}".format(i, v.not_message) for i, v in enumerate(self.values)
+        )
+        self.err_message = "must pass at least one of validations for {}".format(
+            self.__class__.__name__, err_msg
+        )
+        self.not_message = "must fail all validations for {}: {}".format(
+            self.__class__.__name__, not_msg
+        )
+
+    def __call__(self, value):
+        for v in self.values:
+            if v(value):
+                return True
+        return False
+
+
+class All(Validator):
+    def __init__(self, *values):
+        self.values = values
+
+        err_msg = " AND ".join(
+            "({}) {}".format(i, v.err_message) for i, v in enumerate(self.values)
+        )
+        not_msg = " OR ".join(
+            "({}) {}".format(i, v.not_message) for i, v in enumerate(self.values)
+        )
+        self.err_message = "must pass all of validations for {}: {}".format(
+            self.__class__.__name__, err_msg
+        )
+        self.not_message = "must fail at least one of validations for {}: {}".format(
+            self.__class__.__name__, not_msg
+        )
+
+    def __call__(self, value):
+        return all([v(value) for v in self.values])
+
+
+class AnyInstanceOf(Any):
+    def __init__(self, *types):
+        super().__init__(*[InstanceOf(t) for t in types])
+
+
+class AnySubclassOf(Any):
+    def __init__(self, *types):
+        super().__init__(*[SubclassOf(t) for t in types])
